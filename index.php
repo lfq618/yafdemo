0da1c13caddc316bbaf012cf7324391af5e50d97
lfq618 <lfq618@gmail.com>
2015-12-26 02:05:34 +0800

    init

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-echo phpinfo();
+
+define('APPLICATION_PATH', dirname(__FILE__));
+
+$application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
+
+$application->bootstrap()->run();
+?>

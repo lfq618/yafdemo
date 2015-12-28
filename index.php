0afaf52bd993dad540526755518ab6cd184fb9a9
lifuqiang <lifuqiang618@163.com>
2015-12-28 18:00:52 +0800

    fdfd

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
-
 define('APPLICATION_PATH', dirname(__FILE__));
+
+xhprof_enable(XHPROF_FLAGS_NO_BUILTINS | XHPROF_FLAGS_CPU | XHPROF_FLAGS_MEMORY, array(
+       'ignored_functions' => array(
+	        'call_user_func',
+            'call_user_func_array',
+        )
+));
 
 $application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
 
 $application->bootstrap()->run();
+
+
+include_once '/home/wwwroot/effect/xhprof_lib/utils/xhprof_lib.php';
+include_once '/home/wwwroot/effect/xhprof_lib/utils/xhprof_runs.php';
+
+$xhprof_runs = new XHProfRuns_Default();
+$run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_testing");
+echo "http://effect.demo//xhprof_html/index.php?run={$run_id}&source=xhprof_testing\n";
 ?>

cd30542fce7afce83a9daadd8458911cc72c3321
lifuqiang <lifuqiang618@163.com>
2015-12-29 12:14:25 +0800

    fdf

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -8,6 +8,8 @@ class TestController extends Yaf_Controller_Abstract {
 	
 	public function indexAction() {
 		
+		$app = new Yaf_Application();
+		var_dump($app->getConfig('application'));
 		$model = new BaseModel();
 		$str = $model->getDataById(1);
 		echo $str; exit;

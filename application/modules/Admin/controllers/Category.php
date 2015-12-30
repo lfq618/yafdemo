90cdb3ec8c727366bcbf9293e72878f290df39c3
lifuqiang <lifuqiang618@163.com>
2015-12-30 17:39:38 +0800

    ff

--- application/modules/Admin/controllers/Category.php
+++ application/modules/Admin/controllers/Category.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * 文章分类管理
+ * @author lifuqiang
+ *
+ */
 class CategoryController extends Yaf_Controller_Abstract
 {
 	
@@ -9,4 +14,20 @@ class CategoryController extends Yaf_Controller_Abstract
 		var_dump($list);
 		exit;
 	}
+	
+	public function addAction()
+	{
+		$categoryObj = new CategoryModel();
+		
+		if ($this->getRequest()->isPost())
+		{
+			//添加操作处理
+			var_dump($this->getRequest()->getParams());
+			exit;
+		}
+		
+// 		$this->getView()->assign('list', $list);
+	}
+	
+	
 }
